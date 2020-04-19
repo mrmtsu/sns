ed7553836d2e4dc38ecfd840e181853f02c281de
mrmtsu <81202muramatsu@gmail.com>
2020-04-20 00:54:46 +0900

    login

--- snsapp/views.py
+++ snsapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -12,7 +14,23 @@ def signupfunc(request):
           return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
       except:
           user = User.objects.create_user(username2, '', password2)
-      user = User.objects.create_user(username2, '', password2)
-      return render(request, 'signup.html', {'some':100})
+          return render(request, 'signup.html', {'some':100})
 
     return render(request, 'signup.html', {'some':100})
+
+def loginfunc(request):
+    if request.method == 'POST':
+      username2 = request.POST['username']
+      password2 = request.POST['password']
+      user = authenticate(request, username=username2, password=password2)
+      if user is not None:
+        login(request, user)
+        return render(request, 'signup.html')
+      else:
+        return redirect('login')
+    return render(request, 'login.html')
+
+def listfunc(request):
+    return render(request, 'list.html')
+    
+    
