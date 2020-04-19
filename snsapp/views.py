cf1fefcd6870456691f942b6cdefaa8e5785b7cc
mrmtsu <81202muramatsu@gmail.com>
2020-04-19 23:56:04 +0900

    admin

--- snsapp/views.py
+++ snsapp/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def signupfunc(request):
     if request.method == 'POST':
-      username = request.POST['username']
-      print(request.POST)
+      username2 = request.POST['username']
+      password2 = request.POST['password']
+      try:
+          User.objects.get(username=username2)
+          return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
+      except:
+          user = User.objects.create_user(username2, '', password2)
+      user = User.objects.create_user(username2, '', password2)
       return render(request, 'signup.html', {'some':100})
 
     return render(request, 'signup.html', {'some':100})
